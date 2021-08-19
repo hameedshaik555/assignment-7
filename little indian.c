819cdbbfc6fbcae756c2e445adb47c02f796e067
hameedshaik555 <88450801+hameedshaik555@users.noreply.github.com>
2021-08-19 21:16:30 +0530

    Create little indian.c

---
+++ little indian.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    char *p =(char*)&a;
+    if(*p==1)
+    {
+        printf("CPU is little endian");
+
+    }
+    else
+    {
+        printf("CPU is big endian");
+    }
+    return 0;
+}
